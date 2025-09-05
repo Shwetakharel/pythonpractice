60e4d930dfe22c64eabdb9a321ad03e100be7639
Shwetakharel <kharelshweta@gmail.com>
2025-09-05 14:38:20 -0400

    Calculating area of traingle

---
+++ Areaoftraingle.py
@@ -0,0 +1,5 @@
+# Write a Python program to find the area of a triangle.
+l = float(input("Enter the length : "))
+b = float(input("Enter the breadth : "))
+area = 0.5 * l * b
+print(f"Area of traingle with length : {l} and breadth : {b} is {area}")
