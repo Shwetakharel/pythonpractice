3128da5a5b0e62b92ac7898baaec61186210301d
Shwetakharel <kharelshweta@gmail.com>
2025-09-05 14:41:48 -0400

    Generating random numbers and swapping variables

---
+++ GenerateRandomNumbers.py
@@ -0,0 +1,4 @@
+# Write a python program to generate a random numbers
+import numpy as np
+a = np.random.randint(10)
+print(a)
---
+++ Swaptwovariables.py
@@ -0,0 +1,8 @@
+# Write a Python program to swap two variables.
+a = 2
+b = 3
+print(f"Original value of a : {a} and b : {b}")
+c = a
+a = b
+b = c
+print(f"After swapping a : {a} and b : {b}")
