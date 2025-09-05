0e0ae7dab796e5e8c512a07bdc486bc1168514d1
Shwetakharel <kharelshweta@gmail.com>
2025-09-05 14:53:34 -0400

    Conversion

---
+++ celsiustofahreheit.py
@@ -0,0 +1,7 @@
+
+# Write a Python program to convert Celsius to Fahrenheit.
+# F = (°C*9/5)+32
+
+c = float(input("Enter the temperature : "))
+f = (c * 9/5) + 32
+print(f"Temperature in celsius : {c} and fahreheit : {f}")
---
+++ kmtomiles.py
@@ -0,0 +1,6 @@
+# Write a Python program to convert kilometers to miles.
+# km → miles: miles = km × 0.621371
+
+km = float(input("Enter the distance: "))
+calculate = km * 0.621371
+print(f"Distance in kilometer is {km} and distance in mile is {calculate}")
