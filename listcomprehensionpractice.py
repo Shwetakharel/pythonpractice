9a131d914e11eb559173caed44f98b1f8d363391
Shwetakharel <kharelshweta@gmail.com>
2025-09-10 15:40:15 -0400

    Practicing List comprehension

--- listcomprehensionpractice.py
+++ listcomprehensionpractice.py
@@ -1,14 +1,14 @@
 # find languages which start with letter p
-# languages = ['java', 'python', 'php', 'c', 'javascript', 'c++']
-# result = [i for i in languages if i[0] == 'p']
-# print(result)
+languages = ['java', 'python', 'php', 'c', 'javascript', 'c++']
+result = [i for i in languages if i[0] == 'p']
+print(result)
 
 
 # add new list from my_fruits and items if the fruit exists in basket and also starts with 'a'
-# basket = ['apple', 'guava', 'cherry', 'banana']
-# my_fruits = ['apple', 'kiwi', 'grapes', 'banana']
-# new_list = [i for i in my_fruits if i in basket if i.startswith("a")]
-# print(new_list)
+basket = ['apple', 'guava', 'cherry', 'banana']
+my_fruits = ['apple', 'kiwi', 'grapes', 'banana']
+new_list = [i for i in my_fruits if i in basket if i.startswith("a")]
+print(new_list)
 
 
 # Example : accepts items with the small letter “o” in the new list
